9940102ad74decc002dfcc7db9616d411e185900
smallsopi <40257318+smallsopi@users.noreply.github.com>
2020-05-07 20:07:06 +0800

    Add files via upload

--- main/outTec.py
+++ main/outTec.py
@@ -7,47 +7,47 @@
 import numpy
 import allClass
 
-def outTecplot(gridInIn,far,status,fileName):
-    outPoint = numpy.zeros((gridInIn.num[0],7))
+def outTecplot(gridIn,far,status,fileName):
+    outPoint = numpy.zeros((gridIn.num[0],7))
 
 
-    for i in range(gridInIn.num[2]):
+    for i in range(gridIn.num[2]):
         for j in range(3):
-            point = int(gridInIn.status[i,j])
-            outPoint[point,2] += gridInIn.volumn[i]
-            outPoint[point,3] += gridInIn.volumn[i] * status.Phy[i,0]     #U
-            outPoint[point,4] += gridInIn.volumn[i] * status.Phy[i,1]     #V
-            outPoint[point,5] += gridInIn.volumn[i] * status.Phy[i,2]     #P
-            outPoint[point,6] += gridInIn.volumn[i] * status.Phy[i,3]     #Rho
+            point = int(gridIn.status[i,j])
+            outPoint[point,2] += gridIn.volumn[i]
+            outPoint[point,3] += gridIn.volumn[i] * status.Phy[i,0]     #U
+            outPoint[point,4] += gridIn.volumn[i] * status.Phy[i,1]     #V
+            outPoint[point,5] += gridIn.volumn[i] * status.Phy[i,2]     #P
+            outPoint[point,6] += gridIn.volumn[i] * status.Phy[i,3]     #Rho
     
-    for k in range(gridInIn.num[0]):
+    for k in range(gridIn.num[0]):
         outPoint[k,3] = outPoint[k,3] / outPoint[k,2]
         outPoint[k,4] = outPoint[k,4] / outPoint[k,2]
         outPoint[k,5] = outPoint[k,5] / outPoint[k,2]
         outPoint[k,6] = outPoint[k,6] / outPoint[k,2]
     
-    outPoint[:,0] = gridInIn.point[:,0]
-    outPoint[:,1] = gridInIn.point[:,1]
+    outPoint[:,0] = gridIn.point[:,0]
+    outPoint[:,1] = gridIn.point[:,1]
 
     fid = open(fileName,'w')
 
     raw = f"""
 Title = "20200308 0.8Ma 3deg"
 Variables= "X","Y","volumn","U","V","P","Density"
-Zone N={gridInIn.num[0]},E={gridInIn.num[2]},F=FEPOINT, ET=TRIANGLE
+Zone N={gridIn.num[0]},E={gridIn.num[2]},F=FEPOINT, ET=TRIANGLE
     """
 
     fid.write(raw)
 
-    for i in range(gridInIn.num[0]):
+    for i in range(gridIn.num[0]):
         for j in range(7):
             fid.write("%.6f" %outPoint[i,j])
             fid.write('\t')
         fid.write('\n')
     
-    for i in range(gridInIn.num[2]):
+    for i in range(gridIn.num[2]):
         for j in range(3):
-            fid.write(str(gridInIn.status[i,j] + 1))
+            fid.write(str(gridIn.status[i,j] + 1))
             fid.write('\t')
         fid.write('\n')
 
